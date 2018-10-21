64fbc1f012cb972fcaad8d010b475f29442f4728
jsmvalente <jvalente96@gmail.com>
2018-10-21 13:12:49 +0100

    preparing

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import rpc_pb2 as ln
-import rpc_pb2_grpc as lnrpc
+import codecs
 import grpc
 import os
+import rpc_pb2 as ln
+import rpc_pb2_grpc as lnrpc
 
-# Due to updated ECDSA generated tls.cert we need to let gprc know that
-# we need to use that cipher suite otherwise there will be a handhsake
-# error when we communicate with the lnd rpc server.
-os.environ["GRPC_SSL_CIPHER_SUITES"] = 'HIGH+ECDSA'
+macaroon = codecs.encode(open('~/.lnd/data/chain/bitcoin/mainnet/admin.macaroon', 'rb').read(), 'hex')
+os.environ['GRPC_SSL_CIPHER_SUITES'] = 'HIGH+ECDSA'
+cert = open('~/.lnd/tls.cert', 'rb').read()
+ssl_creds = grpc.ssl_channel_credentials(cert)
+channel = grpc.secure_channel('localhost:10009', ssl_creds)
 
-# Lnd cert is at ~/.lnd/tls.cert on Linux and
-# ~/Library/Application Support/Lnd/tls.cert on Mac
-cert = open(os.path.expanduser('~/.lnd/tls.cert'), 'rb').read()
-creds = grpc.ssl_channel_credentials(cert)
-channel = grpc.secure_channel('localhost:10009', creds)
+# Creating a stub and requesting the client for the latest network graph
 stub = lnrpc.LightningStub(channel)
+request = ln.ChannelGraphRequest(include_unannounced=True)
+response = stub.DescribeGraph(request, metadata=[('macaroon'), macaroon])
+print(response)
