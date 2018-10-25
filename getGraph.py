32e0d5d8c77f35c1186baf7072372d33baabd214
jsmvalente <jvalente96@gmail.com>
2018-10-25 19:16:14 +0100

    got the graph and the node_pub_key-alias relation files. still not tested with networkx

--- getGraph.py
+++ getGraph.py
@@ -3,6 +3,7 @@ import grpc
 import os
 import rpc_pb2 as ln
 import rpc_pb2_grpc as lnrpc
+from collections import defaultdict
 
 macaroon = codecs.encode(open('/home/joao/.lnd/data/chain/bitcoin/mainnet/admin.macaroon', 'rb').read(), 'hex')
 os.environ['GRPC_SSL_CIPHER_SUITES'] = 'HIGH+ECDSA'
@@ -14,4 +15,30 @@ channel = grpc.secure_channel('localhost:10009', ssl_creds)
 stub = lnrpc.LightningStub(channel)
 request = ln.ChannelGraphRequest(include_unannounced=True)
 response = stub.DescribeGraph(request, metadata=[('macaroon', macaroon)])
-print(response)
+
+# Treat the response and build a multi line adjancency list from it
+# https://networkx.github.io/documentation/stable/reference/readwrite/multiline_adjlist.html
+adjListDic = defaultdict(list)
+
+for edge in response.edges:
+    # Build adj list from edges
+    adjListDic[edge.node1_pub].append([edge.node2_pub, edge.capacity])
+    adjListDic[edge.node2_pub].append([edge.node1_pub, edge.capacity])
+
+# Write the adjlist file
+f = open('lightningMLAdjList', 'w')
+for (nodePubKey, adjList) in adjListDic.items():
+    # Write the name of the node and the number of edges
+    f.write(nodePubKey + " " + str(len(adjList)) + "\n")
+    for adj in adjList:
+        # Write the other node in the edge and the edge capacity
+        f.write(adj[0] + " {'capacity':" + str(adj[1]/2) + "}\n")
+
+f.close()
+
+# Write a file with the correspondence between the nodes alias and pubkeys
+f = open('nodeAlias.text', 'w')
+for node in response.nodes:
+    f.write(node.pub_key + " " + node.alias + "\n")
+
+f.close()
