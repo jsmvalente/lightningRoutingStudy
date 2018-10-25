3d5788f925a6f1b51993c2102daf4b5402cb4e0a
jsmvalente <jvalente96@gmail.com>
2018-10-25 11:41:12 +0100

    GOT THE GRAPHgit status still need to add make it netx compatible though

--- getGraph.py
+++ getGraph.py
@@ -4,14 +4,14 @@ import os
 import rpc_pb2 as ln
 import rpc_pb2_grpc as lnrpc
 
-macaroon = codecs.encode(open('~/.lnd/data/chain/bitcoin/mainnet/admin.macaroon', 'rb').read(), 'hex')
+macaroon = codecs.encode(open('/home/joao/.lnd/data/chain/bitcoin/mainnet/admin.macaroon', 'rb').read(), 'hex')
 os.environ['GRPC_SSL_CIPHER_SUITES'] = 'HIGH+ECDSA'
-cert = open('~/.lnd/tls.cert', 'rb').read()
+cert = open('/home/joao/.lnd/tls.cert', 'rb').read()
 ssl_creds = grpc.ssl_channel_credentials(cert)
 channel = grpc.secure_channel('localhost:10009', ssl_creds)
 
 # Creating a stub and requesting the client for the latest network graph
 stub = lnrpc.LightningStub(channel)
 request = ln.ChannelGraphRequest(include_unannounced=True)
-response = stub.DescribeGraph(request, metadata=[('macaroon'), macaroon])
+response = stub.DescribeGraph(request, metadata=[('macaroon', macaroon)])
 print(response)
