8e940321edddbc109725af871f2c5cb05c6f596c
jsmvalente <jvalente96@gmail.com>
2018-10-26 04:08:27 +0100

    fix building graph

--- getGraph.py
+++ getGraph.py
@@ -24,16 +24,28 @@ adjListDic = defaultdict(list)
 for node in response.nodes:
     adjListDic[node.pub_key] = []
 
-print("Got " + str(len(response.edges)) + " edges")
-print("Got " + str(len(response.nodes)) + " nodes")
-
+# Build adj list from edges
 for edge in response.edges:
-    # Build adj list from edges
-    adjListDic[edge.node1_pub].append([edge.node2_pub, edge.capacity])
-    adjListDic[edge.node2_pub].append([edge.node1_pub, edge.capacity])
+    # Find if this edge already exists
+    if any(adj[0] == edge.node2_pub for adj in adjListDic[edge.node1_pub]):
+        # If it exists we add to that already existing capacity for both directions
+        for (index, adj) in enumerate(adjListDic[edge.node1_pub]):
+            if adj[0] == edge.node2_pub:
+                adjListDic[edge.node1_pub][index][1] += edge.capacity
+                break
+        for (index, adj) in enumerate(adjListDic[edge.node2_pub]):
+            if adj[0] == edge.node1_pub:
+                adjListDic[edge.node2_pub][index][1] += edge.capacity
+                break
+    # IF it doesn't exist we add a new adjacency
+    else:
+        adjListDic[edge.node1_pub].append([edge.node2_pub, edge.capacity])
+        adjListDic[edge.node2_pub].append([edge.node1_pub, edge.capacity])
+
 
 # Write the adjlist file
 f = open('lightningAdjList.txt', 'w')
+repeated = 0
 for (nodePubKey, adjList) in adjListDic.items():
     # Write the name of the node and the number of edges
     f.write(nodePubKey + " " + str(len(adjList)) + "\n")
